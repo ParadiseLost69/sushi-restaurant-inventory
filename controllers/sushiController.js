cc03f1300aaf70c3bda5dcb8ea79b529e9ac13ff
Teddy <tedloukas@outlook.com>
2022-12-28 17:33:04 -0500

    add PUT functionality

--- controllers/sushiController.js
+++ controllers/sushiController.js
@@ -27,8 +27,14 @@ exports.sushi_create_get = (req, res) => {
 };
 
 exports.sushi_create_post = [
-  body("name", "Name is required").trim().isLength({ min: 2 }).escape(),
-  body("price", "price is required").trim().isLength({ min: 1 }).escape(),
+  body("name", "Invalid Name: Must be at least 2 letters")
+    .trim()
+    .isLength({ min: 2 })
+    .escape(),
+  body("price", "Invalid Price: Price must be entered")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
   //   body("description").trim().escape(),
   //   body("stock").trim().escape(),
   //   body("category").trim().escape(),
@@ -71,10 +77,29 @@ exports.sushi_delete_post = (req, res) => {
   });
 };
 
-exports.sushi_update_get = (req, res) => {
-  res.send("get da sushi update");
+exports.sushi_update_get = (req, res, next) => {
+  Sushi.findById(req.params.id, (err, results) => {
+    if (err) {
+      return next(err);
+    }
+    res.render("sushi_update", { title: "Update the sushi", sushi: results });
+  });
 };
 
-exports.sushi_update_post = (req, res) => {
-  res.send("Update the sushi");
+exports.sushi_update_post = (req, res, next) => {
+  console.log(req.body.name);
+  const sushi = new Sushi({
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+    stock: req.body.stock,
+    category: req.body.category,
+    _id: req.params.id,
+  });
+  Sushi.findByIdAndUpdate(req.params.id, sushi, {}, (err, results) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/sushi");
+  });
 };
